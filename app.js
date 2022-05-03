e97ed41009c1d0614c2a00f044912965550105f8
Ilya Barinov <ilia.barinov2012@gmail.com>
2022-05-04 02:15:24 +0500

    ADD fetch + some socket
    По ссылке results добавлены fetch с выводом данных
    Попытка реализовать socket

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 const express = require('express');
 const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http)
+
 const mainRouter = require('./routes/index')
 
 app.use(express.json())
 app.use(mainRouter)
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
+});
+
 app.listen(8081, 'localhost', () => {
     console.log('Server running at http://127.0.0.1:8081/');
 })
--- pages/results/js/main.js
+++ pages/results/js/main.js
@@ -1,22 +1,15 @@
-const api_results = new XMLHttpRequest();
-api_results.open("GET", "/api/results", true);
-api_results.send()
-
-api_results.onreadystatechange = function () {
-    if (api_results.readyState != 4) {
-        return
+fetch('/api/results', { method: 'GET'})
+    .then(res => res.json())
+    .then(function (json) {
+        table = document.querySelector(".table_result");
+        for (var i in json) {
+            var v = json[i];
+            table.innerHTML += `<tr><td>${v.id}</td> <td>${v.winner}</td> <td>${v.loser}</td></tr>`
+        }
     }
-
-    table = document.querySelector(".table_result");
-    json_data = JSON.parse(api_results.responseText)
-    for (var i in json_data) {
-        var v = json_data[i];
-        table.innerHTML += `<tr><td>${v.id}</td> <td>${v.winner}</td> <td>${v.loser}</td></tr>`
-    }
-}
+);
 
 function clean() {
-    api_results.open("DELETE", "/api/results", true);
-    api_results.send()
+    fetch('/api/results', { method: 'DELETE'});
     location.reload();
 }
